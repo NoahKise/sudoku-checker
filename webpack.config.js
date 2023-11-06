095127c10f5ed0e46180238178bf71f66c0a2174
CalvinPVIII <calvinp003@gmail.com>
2023-11-06 12:12:34 -0800

    update README

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Project",
+      title: "Shape Tracker",
       template: "./src/index.html",
       inject: "body",
     }),
