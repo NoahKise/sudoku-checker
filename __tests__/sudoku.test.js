537e5a3066a7c923781a054e89d254c2708198d8
noahkise <noah@kisefamily.com>
2023-11-07 11:13:59 -0800

    good fail winCheck test 2
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -12,24 +12,24 @@ describe('SudokuBoard', () => {
         expect(sudokuBoard.row7).toEqual([]);
         expect(sudokuBoard.row8).toEqual([]);
         expect(sudokuBoard.row9).toEqual([]);
-        expect(sudokuBoard.col1).toEqual([]);
-        expect(sudokuBoard.col2).toEqual([]);
-        expect(sudokuBoard.col3).toEqual([]);
-        expect(sudokuBoard.col4).toEqual([]);
-        expect(sudokuBoard.col5).toEqual([]);
-        expect(sudokuBoard.col6).toEqual([]);
-        expect(sudokuBoard.col7).toEqual([]);
-        expect(sudokuBoard.col8).toEqual([]);
-        expect(sudokuBoard.col9).toEqual([]);
-        expect(sudokuBoard.box1).toEqual([]);
-        expect(sudokuBoard.box2).toEqual([]);
-        expect(sudokuBoard.box3).toEqual([]);
-        expect(sudokuBoard.box4).toEqual([]);
-        expect(sudokuBoard.box5).toEqual([]);
-        expect(sudokuBoard.box6).toEqual([]);
-        expect(sudokuBoard.box7).toEqual([]);
-        expect(sudokuBoard.box8).toEqual([]);
-        expect(sudokuBoard.box9).toEqual([]);
+        expect(sudokuBoard.row10).toEqual([]);
+        expect(sudokuBoard.row11).toEqual([]);
+        expect(sudokuBoard.row12).toEqual([]);
+        expect(sudokuBoard.row13).toEqual([]);
+        expect(sudokuBoard.row14).toEqual([]);
+        expect(sudokuBoard.row15).toEqual([]);
+        expect(sudokuBoard.row16).toEqual([]);
+        expect(sudokuBoard.row17).toEqual([]);
+        expect(sudokuBoard.row18).toEqual([]);
+        expect(sudokuBoard.row19).toEqual([]);
+        expect(sudokuBoard.row20).toEqual([]);
+        expect(sudokuBoard.row21).toEqual([]);
+        expect(sudokuBoard.row22).toEqual([]);
+        expect(sudokuBoard.row23).toEqual([]);
+        expect(sudokuBoard.row24).toEqual([]);
+        expect(sudokuBoard.row25).toEqual([]);
+        expect(sudokuBoard.row26).toEqual([]);
+        expect(sudokuBoard.row27).toEqual([]);
     });
 });
 
@@ -39,10 +39,13 @@ describe('SudokuBoard.prototype.winCheck', () => {
     
     beforeEach(() => {
         reusableSudoku = new SudokuBoard([4, 3, 5, 2, 6, 9, 7, 8, 1,], [6, 8, 2, 5, 7, 1, 4, 9, 3,], [1, 9, 7, 8, 3, 4, 5, 6, 2], [8, 2, 6, 1, 9, 5, 3, 4, 7], [3, 7, 4, 6, 8, 2, 9, 1, 5], [9, 5, 1, 7, 4, 3, 6, 2, 8], [5, 1, 9, 3, 2, 6, 8, 7, 4], [2, 4, 8, 9, 5, 7, 1, 3, 6], [7, 6, 3, 4, 1, 8, 2, 5, 9], [4, 6, 1, 8, 3, 9, 5, 2, 7], [3, 8, 9, 2, 7, 5, 1, 4, 6], [5, 2, 7, 6, 4, 1, 9, 8, 3], [2, 5, 8, 1, 6, 7, 3, 9, 4], [6, 7, 3, 9, 8, 4, 2, 5, 1], [9, 1, 4, 5, 2, 3, 6, 7, 8], [7, 4, 5, 3, 9, 6, 8, 1, 2], [8, 9, 6, 4, 1, 2, 7, 3, 5], [1, 3, 2, 7, 5, 8, 4, 6, 9], [4, 3, 5, 6, 8, 2, 1, 9, 7], [2, 6, 9, 5, 7, 1, 8, 3, 4,], [7, 8, 1, 4, 9, 3, 5, 6, 2], [8, 2, 6, 3, 7, 4, 9, 5, 1], [1, 9, 5, 6, 8, 2, 7, 4, 3], [3, 4, 7, 9, 1, 5, 6, 2, 8], [5, 1, 9, 2, 4, 8, 7, 6, 3], [3, 2, 6, 9, 5, 7, 4, 1, 8], [8, 7, 4, 1, 3, 6, 2, 5, 9]);
-        reusableTestArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     });
 
     test('should correctly determine if sudokuBoard.row1 contains each intiger from 1 to 9', () => {
+        let result = reusableSudoku.winCheck();
+        expect(result).toEqual(true);
+    })
+    test('should correctly determine if every property of sudokuBoard contains each intiger from 1 to 9', () => {
         let result = reusableSudoku.winCheck();
         expect(result).toEqual(true);
     })
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,4 +1,4 @@
-export default function SudokuBoard(row1, row2, row3, row4, row5, row6, row7, row8, row9, col1, col2, col3, col4, col5, col6, col7, col8, col9, box1, box2, box3, box4, box5, box6, box7, box8, box9){
+export default function SudokuBoard(row1, row2, row3, row4, row5, row6, row7, row8, row9, row10, row11, row12, row13, row14, row15, row16, row17, row18, row19, row20, row21, row22, row23, row24, row25, row26, row27) {
     this.row1 = row1;
     this.row2 = row2;
     this.row3 = row3;
@@ -8,29 +8,36 @@ export default function SudokuBoard(row1, row2, row3, row4, row5, row6, row7, ro
     this.row7 = row7;
     this.row8 = row8;
     this.row9 = row9;
-    this.col1 = col1;
-    this.col2 = col2;
-    this.col3 = col3;
-    this.col4 = col4;
-    this.col5 = col5;
-    this.col6 = col6;
-    this.col7 = col7;
-    this.col8 = col8;
-    this.col9 = col9;
-    this.box1 = box1;
-    this.box2 = box2;
-    this.box3 = box3;
-    this.box4 = box4;
-    this.box5 = box5;
-    this.box6 = box6;
-    this.box7 = box7;
-    this.box8 = box8;
-    this.box9 = box9;
+    this.row10 = row10;
+    this.row11 = row11;
+    this.row12 = row12;
+    this.row13 = row13;
+    this.row14 = row14;
+    this.row15 = row15;
+    this.row16 = row16;
+    this.row17 = row17;
+    this.row18 = row18;
+    this.row19 = row19;
+    this.row20 = row20;
+    this.row21 = row21;
+    this.row22 = row22;
+    this.row23 = row23;
+    this.row24 = row24;
+    this.row25 = row25;
+    this.row26 = row26;
+    this.row27 = row27;
 }
 
 SudokuBoard.prototype.winCheck = function () {
     const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let result = testArray.every(i => this.row1.includes(i));
+    let result = true;
+
+    for (let i = 0; i < 27; i++) {
+        if (!testArray.every(j => this[`row${i + 1}`].includes(j))); {
+            result = false;
+            break;
+        }
+    }
     return result;
 }
 
