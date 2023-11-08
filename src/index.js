5435e22813a3990d902f3ef786b74cfdeefcbf8e
noahkise <noah@kisefamily.com>
2023-11-07 16:17:23 -0800

    add values to form inputs
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ function codeBoard(e) {
 
   const inputBoard = new SudokuBoard([c0, c1, c2, c3, c4, c5, c6, c7, c8], [c9, c10, c11, c12, c13, c14, c15, c16, c17], [c18, c19, c20, c21, c22, c23, c24, c25, c26], [c27, c28, c29, c30, c31, c32, c33, c34, c35], [c36, c37, c38, c39, c40, c41, c42, c43, c44], [c45, c46, c47, c48, c49, c50, c51, c52, c53], [c54, c55, c56, c57, c58, c59, c60, c61, c62], [c63, c64, c65, c66, c67, c68, c69, c70, c71], [c72, c73, c74, c75, c76, c77, c78, c79, c80], [c0, c9, c18, c27, c36, c45, c54, c63, c72], [c1, c10, c19, c28, c37, c46, c55, c64, c73], [c2, c11, c20, c29, c38, c47, c56, c65, c74], [c3, c12, c21, c30, c39, c48, c57, c66, c75], [c4, c13, c22, c31, c40, c49, c58, c67, c76], [c5, c14, c23, c32, c41, c50, c59, c68, c77], [c6, c15, c24, c33, c42, c51, c60, c69, c78], [c7, c16, c25, c34, c43, c52, c61, c70, c79], [c8, c17, c26, c33, c44, c53, c62, c71, c80], [c0, c1, c2, c9, c10, c11, c18, c19, c20], [c3, c4, c5, c12, c13, c14, c21, c22, c23], [c6, c7, c8, c15, c16, c17, c24, c25, c26], [c27, c28, c29, c36, c37, c38, c45, c46, c47], [c30, c31, c32, c39, c40, c41, c48, c49, c50], [c33, c34, c35, c42, c43, c44, c51, c52, c53], [c54, c55, c56, c63, c64, c65, c72, c73, c74], [c57, c58, c59, c66, c67, c68, c75, c76, c77], [c60, c61, c62, c69, c70, c71, c78, c79, c80]);
 
-  console.log(inputBoard.winCheck());
+  console.log(inputBoard);
 
   let result = inputBoard.winCheck();
   return result;
